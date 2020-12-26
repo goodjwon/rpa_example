65b0e58ff6b798cfbfbc25e50ecc88fb4befc5f8
JungWon Park <firstman9@protonmail.com>
2020-12-26 10:58:01 +0900

    excel 파일 만들기 => 셀범위 핸들링

--- 1_excel/5_cell_range.py
+++ 1_excel/5_cell_range.py
@@ -1,5 +1,7 @@
 from openpyxl import Workbook
 from random import *
+from openpyxl.utils.cell import coordinate_from_string
+
 wb = Workbook()
 ws = wb.active
 
@@ -29,10 +31,43 @@ for cell in row_title:
 print("==========get Rows================")
 row_range = ws[2:6]
 for rows in row_range:
-
     for cell in rows:
         print(cell.value, end=" ")
     print()
 
+print("==========get MaxRows==============")
+row_range = ws[2: ws.max_row]
+for rows in row_range:
+    for cell in rows:
+        # print(cell.value, end=" ")
+        print(cell.coordinate, end=" ")
+        xy = coordinate_from_string(cell.coordinate)
+        print(xy)
+    print()
 
-#wb.save("sample4.xlsx")
+print("==========get AllRows==============")
+print(tuple(ws.rows))
+
+for row in tuple(ws.rows):
+    print(row[1].value)
+
+for row in ws.iter_rows():
+    print(row[1].value)
+
+
+print("==========get AllColumns==============")
+print(tuple(ws.columns))
+
+for column in tuple(ws.columns):
+    print(column[0].value)
+
+for column in ws.iter_cols():
+    print(column[0].value)
+
+print("==========get 1row to 5 and 2 col to 3 =====")
+for row in ws.iter_rows(min_row=2, max_row=11, min_col=2, max_col=3):
+    print(row[0].value, row[1].value) # 수학, 영어
+
+
+
+wb.save("sample4.xlsx")
