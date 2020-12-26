ce8dc2ef636259443a89b3bcda258d6906ec5235
JungWon Park <firstman9@protonmail.com>
2020-12-27 08:58:06 +0900

    행맨 프로그램 작성 (리팩토링 하지 않음)

---
+++ 5_hangman/hangman.py
@@ -0,0 +1,30 @@
+from random import *
+words = ["apple", "banana", "coffee"]
+word = choice(words)
+print("answer is", word)
+letters = ""
+
+while True:
+    succeed  = True
+    print()
+    for w in word:
+        if w in letters:
+            print(w, end=" ")
+        else:
+            print("_", end=" ")
+            succed = False
+    print()
+
+    if succeed :
+        print("SUCCESS")
+        break
+
+    letter = input("Input letter > ")
+
+    if letter not in letters:
+        letters += letter
+
+    if letter in word:
+        print("Currect")
+    else:
+        print("Wrong")
