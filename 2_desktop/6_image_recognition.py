3bc85a81204907d651ef27de6f8d9a226bd1a4ff
JungWon Park <firstman9@protonmail.com>
2021-01-03 11:22:40 +0900

    pyautogui - 마우스 이벤트 - 스크린 처리 - 이미지 클립으로 마우스 위치정보 확인 - 함수로 변경

--- 2_desktop/6_image_recognition.py
+++ 2_desktop/6_image_recognition.py
@@ -1,3 +1,5 @@
+import time
+import sys
 import pyautogui
 
 # file_menu = pyautogui.locateOnScreen("file_menu.png")
@@ -14,13 +16,49 @@ import pyautogui
         => locateOnScreen("file_menu.png", confidence=0.9)
     4. 참고 : numpy 1.19.4 버그 있음 numpy 1.19.3 사용.
 """
+# file_menu_notepad = None
+# while file_menu_notepad is None:
+#     file_menu_notepad = pyautogui.locateOnScreen("note_pad_menu.png")
+#     print("발견실패")
+# pyautogui.click(file_menu_notepad)
 
-file_menu_notepad = pyautogui.locateOnScreen("note_pad_menu.png")
+"""
+    시간 제한 설정하여 코딩.
+"""
+# timeout = 10
+# start = time.time()
+# file_menu_notepad = None
+# while file_menu_notepad is None:
+#     file_menu_notepad = pyautogui.locateOnScreen("note_pad_menu.png")
+#     end = time.time()
+#     print(end, start, end - start)
+#     if end - start > timeout:
+#         print("시간종료")
+#         sys.exit()
+# pyautogui.click(file_menu_notepad)
 
-while file_menu_notepad is None:
-    file_menu_notepad = pyautogui.locateOnScreen("note_pad_menu.png")
+"""
+    함수로 변경
+"""
 
-pyautogui.click(file_menu_notepad)
+def find_target(imge, timeout):
+    start = time.time()
+    target = None
+    while target is None:
+        target = pyautogui.locateOnScreen(imge)
+        end = time.time()
+        if end - start > timeout:
+            break
+    return target
+
+def my_click(image, timeout=30):
+    target = find_target(image, timeout)
+    if target:
+        pyautogui.click(target)
+    else:
+        print("발견실패")
+        print(f"f[Timeout {timeout}s] Target not fount ({image}).")
+        sys.exit()
 
 
-
+my_click("note_pad_menu.png", 10)
